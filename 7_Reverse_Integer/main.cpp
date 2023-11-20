63aa5034cc2fd20d7305d40a72066db74b700811
MD ALIF HOSSAIN KARUZ <karuzm121@gmail.com>
2023-11-21 04:13:53 +0600

    int data type limit check in if condition

--- 7_Reverse_Integer/main.cpp
+++ 7_Reverse_Integer/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        int reminder;
+        long long rev = 0;
+        while (x != 0)
+        {
+            reminder = x % 10;
+            rev = rev * 10 + reminder;
+            x /= 10;
+        }
+        if (rev < -2147483648 || rev > 2147483647)
+            return 0;
+        else
+            return rev;
+    }
+};
 int main()
 {
-    cout<<"Hello";
+    Solution a;
+    a.reverse(-2147483648);
     
 }
