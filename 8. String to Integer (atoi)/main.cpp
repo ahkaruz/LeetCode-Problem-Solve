9f32812c44cc7b9638552670201a180832b48dd1
MD ALIF HOSSAIN KARUZ <karuzm121@gmail.com>
2023-11-21 13:24:52 +0600

    first commit

---
+++ 8. String to Integer (atoi)/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int myAtoi(string s) {
+       
+        cout<<s;
+        return 0;
+    }
+};
+
+int main()
+{
+    Solution s;
+    s.myAtoi("Alif");
+}
